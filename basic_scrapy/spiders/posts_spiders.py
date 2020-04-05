ca432972768f772cc07f2fec689d86e694e981dd
Andrei B <ruby.project15@gmail.com>
2020-04-05 20:27:55 +0300

    looping through all the posts from all pages

--- basic_scrapy/spiders/posts_spiders.py
+++ basic_scrapy/spiders/posts_spiders.py
@@ -8,13 +8,13 @@ class PostsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        #page = response.url.split('/')[-1]
-        #filename = 'posts-%s.html' % page
-        #with open(filename,'wb') as f:
-         #   f.write(response.body)
         for post in response.css('div.post-item'):
             yield {
                 'title': post.css('.post-header h2 a::text')[0].get(),
                 'date': post.css('.post-header a::text')[1].get(),
                 'author': post.css('.post-header a::text')[2].get()
             }
+        next_page = response.css('a.next-posts-link::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
