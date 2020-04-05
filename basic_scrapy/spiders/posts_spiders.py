5547466b0f4abef02b273fe6180d781f44c65f1e
Andrei B <ruby.project15@gmail.com>
2020-04-05 20:08:42 +0300

    getting data from all posts in the main page

--- basic_scrapy/spiders/posts_spiders.py
+++ basic_scrapy/spiders/posts_spiders.py
@@ -4,12 +4,17 @@ class PostsSpider(scrapy.Spider):
     name = 'posts'
 
     start_urls = [
-        'https://blog.scrapinghub.com/page/1',
-        'https://blog.scrapinghub.com/page/2'
+        'https://blog.scrapinghub.com/',
     ]
 
     def parse(self, response):
-        page = response.url.split('/')[-1]
-        filename = 'posts-%s.html' % page
-        with open(filename,'wb') as f:
-            f.write(response.body)
+        #page = response.url.split('/')[-1]
+        #filename = 'posts-%s.html' % page
+        #with open(filename,'wb') as f:
+         #   f.write(response.body)
+        for post in response.css('div.post-item'):
+            yield {
+                'title': post.css('.post-header h2 a::text')[0].get(),
+                'date': post.css('.post-header a::text')[1].get(),
+                'author': post.css('.post-header a::text')[2].get()
+            }
